c2bd681af7d35bb2abadcfb40b01a21abc66f3b0
Joshua Fredrickson <uafredrickson@gmail.com>
2018-06-04 15:02:14 -0700

    06-04-2018  03:01pm  cont. scaffolding

---
+++ front-end/src/component/app/app.js
@@ -0,0 +1,17 @@
+import React form 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Dashoard from '../dashboard/dashboard';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <div className='app'>
+          <BrowserRouter>
+            <div>
+              <Route exact path='/' compoonent={Dashboard} />
+            </div>
+          </BrowserRouter>
+        </div>
+    );
+  }
+}
